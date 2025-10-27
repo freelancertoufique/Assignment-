17d3f9fd2e83515cd4a1c1d450237e19ba204a70
Toufique Ahamed <freelancertoufique@gmail.com>
2025-10-27 18:57:32 +0600

    formated all the doccument

--- assest/js/script.js
+++ assest/js/script.js
@@ -4,24 +4,24 @@ const menuHandle = document.querySelector("#menu")
 
 // making header stiky
 
-window.addEventListener('scroll',()=>{
-    
-    if(window.scrollY > 100){
+window.addEventListener('scroll', () => {
+
+    if (window.scrollY > 100) {
         header.classList.add("active")
-    }else{
+    } else {
         header.classList.remove("active")
     };
 });
 
 // for navigating navbars 
 navigation.forEach(item => {
-  item.addEventListener("click", () => {
-    navigation.forEach(i => i.classList.remove("active"));
-    item.classList.add("active");
-  });
+    item.addEventListener("click", () => {
+        navigation.forEach(i => i.classList.remove("active"));
+        item.classList.add("active");
+    });
 });
 
-menuHandle.addEventListener("click",()=>{
+menuHandle.addEventListener("click", () => {
     const navItem = document.querySelector(".navItem")
     navItem.classList.toggle("active")
 
